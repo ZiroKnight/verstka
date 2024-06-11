ad6d768d1c8d599c6ade796852ca2646566d467f
Vitalii <knifevbn@gmail.com>
2024-06-11 17:18:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#73778E',
+        'gray': '#E9E9F1',
+        'dark-gray': '#8C8D9D',
+        'grad1': '#CDB4DD',
+        'grad2': '#7C77B8',
+        'grad3': '#4AADA7',
+        'white2':'#F2F2F2',
+        'modal':'#ADAACA',
+        'input':'#D9D9D9',
+        'placeholder':'#505050',
+        // 'gray': '#8492a6',
+        // 'gray-light': '#d3dce6',
+      },
+      dropShadow: {
+        '3xl': '0 5px 6px rgba(0, 0, 0, 0.25)',
+      },
+      fontFamily:{
+        nunito: ['Nunito', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
